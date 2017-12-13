653db0b262cf6a903c034e6ccc219dd3b8b21d60
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-12-12 16:50:25 -0800

    update copyright and contact information

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,6 @@
 # Cookbook:: golob-taskmanager
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Fred Hutchinson CRC, All Rights Reserved.
+
+include_recipe 'nginx::default'
