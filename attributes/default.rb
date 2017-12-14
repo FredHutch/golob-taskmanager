3a9ee3d7c9b7f6c7fdcf5d83ec1e1e8c8f459dc2
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-12-13 17:08:03 -0800

    certificate paths need to be fully qualified

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['nginx']['default_site_enabled'] = false
 default['golob-taskmanager'] = {
   'server_name' => 'gt-test',
-  'ssl_cert' => 'cert.pem',
-  'ssl_cert_key' => 'cert_key.pem'
+  'ssl_cert' => '/etc/nginx/cert.pem',
+  'ssl_cert_key' => '/etc/nginx/cert_key.pem'
 }
