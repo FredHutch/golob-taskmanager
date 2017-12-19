94466d9407daf1c8b09fa93980b416022b284bf1
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-12-19 13:00:10 -0800

    Add airflow base configuration

---
+++ attributes/airflow.rb
@@ -0,0 +1,1 @@
+default['airflow']['packages'] = {}
---
+++ recipes/airflow.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: golob-taskmanager
+# Recipe:: airflow
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'airflow::default'
+
+execute 'airflow initdb' do
+  creates "#{node['airflow']['config']['core']['airflow_home']}/airflow.db"
+  command "#{node['airflow']['bin_path']}/airflow initdb"
+  user node['airflow']['user']
+  environment 'AIRFLOW_HOME' => node['airflow']['config']['core']['airflow_home']
+end
+
+include_recipe 'airflow::webserver'
