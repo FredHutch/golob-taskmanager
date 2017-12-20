dd8b95cb8ff6f98edc19977a0952f7de59009117
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-12-20 15:25:23 -0800

    Release 0.2.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mrg@fredhutch.org'
 license 'All Rights Reserved'
 description 'Installs/Configures golob-taskmanager'
 long_description 'Installs/Configures golob-taskmanager'
-version '0.2.2'
+version '0.2.3'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'ubuntu'
 
